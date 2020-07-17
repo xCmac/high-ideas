af3fdbc69ec1b0767e4ef832154ed6934182924f
xCmac <dsemac@gmail.com>
2020-07-17 01:03:02 -0500

    .

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Idea } from './idea.model';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  payloadUrl = 'https://raw.githubusercontent.com/xCmac/high-ideas/master/ideas.json';
 
-  constructor() {}
+  ideas: Idea[];
+  currentIdea: Idea;
+  isShowingIdea = true;
 
+  constructor(private http: HttpClient) {
+    this.http.get(this.payloadUrl).toPromise().then(r => {
+      this.ideas = r as Idea[];
+      this.refreshIdeaClick();
+    });
+  }
+
+  refreshIdeaClick() {
+    console.log('Loading New Idea....');
+    const randomElement = this.ideas[Math.floor(Math.random() * this.ideas.length)];
+    this.currentIdea = randomElement;
+    console.log(this.currentIdea);
+  }
+
+  addIdeaClick() {
+    console.log('Add Idea Clicked');
+  }
+
+  submitIdeaClick() {
+    this.isShowingIdea = false;
+    console.log('Submitting Idea Click');
+  }
+
+  actuallySubmitIdeaClick() {
+    this.isShowingIdea = true;
+  }
+
+  cancelSubmitIdeaClick() {
+    this.isShowingIdea = true;
+  }
 }
---
+++ src/app/home/idea.model.ts
@@ -0,0 +1,4 @@
+export interface Idea {
+    title: string;
+    description: string;
+}
